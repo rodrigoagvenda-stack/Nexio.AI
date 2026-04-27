85d0f9c9ab426908a98c8b8faf99adc4aa2699eb
rodrigoagvenda-stack <rodrigoagvenda-stack@users.noreply.github.com>
2026-04-27 13:34:36 -0300

    fix: build -- NODE_OPTIONS max-old-space-size 1536MB, disable source maps, swcMinify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
